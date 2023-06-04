49040593d405bbccada1447834f3da55ed90941f
Hector Romero <64171601+MeanKid619@users.noreply.github.com>
2023-06-04 17:33:45 -0500

    Update download.sh

--- download.sh
+++ download.sh
@@ -20,26 +20,10 @@ if [ ! -e ~/.wgetrc ] ; then
   echo "check_certificate = off" >> ~/.wgetrc
 fi
 
-echo -e "${WHITE}Lade original APK Version 2.3.0 herunter ...${NORMAL}"
-wget -O APK/apkpure.html --keep-session-cookies --save-cookies cookies.txt -U chrome https://apkpure.com/de/freestyle-librelink-de/com.freestylelibre.app.de/download/4751-APK
-URL=$(grep "hier klicken" APK/apkpure.html | sed 's#^.*https://##' | sed 's/">.*//')
-wget -O APK/${FILENAME_230}.apk --load-cookies cookies.txt -U chrome https://${URL}
-if [ $? = 0 ]; then
-  echo -e "${GREEN}  okay.${NORMAL}"
-  echo
-else
-  echo -e "${RED}  nicht okay.${NORMAL}"
-  echo
-  echo -e "${YELLOW}=> Bitte prüfen Sie o.a. Fehler.${NORMAL}"
-  exit 1
-fi
-rm cookies.txt
-rm APK/apkpure.html
-
-#echo -e "${WHITE}Lade original APK Version 2.4.0 herunter ...${NORMAL}"
-#wget -O APK/apkpure.html --keep-session-cookies --save-cookies cookies.txt https://apkpure.com/de/freestyle-librelink-de/com.freestylelibre.app.de/download/5417-APK
+#echo -e "${WHITE}Lade original APK Version 2.3.0 herunter ...${NORMAL}"
+#wget -O APK/apkpure.html --keep-session-cookies --save-cookies cookies.txt -U chrome https://apkpure.com/de/freestyle-librelink-de/com.freestylelibre.app.de/download/4751-APK
 #URL=$(grep "hier klicken" APK/apkpure.html | sed 's#^.*https://##' | sed 's/">.*//')
-#wget -O APK/${FILENAME_240}.apk --load-cookies cookies.txt https://${URL}
+#wget -O APK/${FILENAME_230}.apk --load-cookies cookies.txt -U chrome https://${URL}
 #if [ $? = 0 ]; then
 #  echo -e "${GREEN}  okay.${NORMAL}"
 #  echo
@@ -51,6 +35,22 @@ rm APK/apkpure.html
 #fi
 #rm cookies.txt
 #rm APK/apkpure.html
+
+echo -e "${WHITE}Lade original APK Version 2.4.0 herunter ...${NORMAL}"
+wget -O APK/apkpure.html --keep-session-cookies --save-cookies cookies.txt https://apkpure.com/de/freestyle-librelink-de/com.freestylelibre.app.de/download/5417-APK
+URL=$(grep "hier klicken" APK/apkpure.html | sed 's#^.*https://##' | sed 's/">.*//')
+wget -O APK/${FILENAME_240}.apk --load-cookies cookies.txt https://${URL}
+if [ $? = 0 ]; then
+  echo -e "${GREEN}  okay.${NORMAL}"
+  echo
+else
+  echo -e "${RED}  nicht okay.${NORMAL}"
+  echo
+  echo -e "${YELLOW}=> Bitte prüfen Sie o.a. Fehler.${NORMAL}"
+  exit 1
+fi
+rm cookies.txt
+rm APK/apkpure.html
 
 echo -e "${WHITE}Lade 'apktool' herunter ...${NORMAL}"
 echo "Info: Debian liefert eine nicht ohne weiteres funktionierende 'dirty'-Version mit. Daher der externe Download."
